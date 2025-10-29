8ee13e1d89cd1961277c1e1dc39de449e26053cd
yg2127 <yg2127@naver.com>
2025-10-29 19:27:31 +0900

    yugeon-1029-1

--- 10291843.py
+++ 10291843.py
@@ -1,11 +1,10 @@
-import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 
 # 0. Google API 키 설정 (사전에 발급받은 키를 환경 변수로 설정)
-# os.environ["GOOGLE_API_KEY"] = "YOUR_GOOGLE_API_KEY"
+# os.environ["GOOGLE_API_KEY"] = "/Users/yugeon/Downloads/opensai-fc28726591b0.json"
 
 # 1. 지식 베이스(텍스트 파일) 로드
 with open('club_info.txt', 'r', encoding='utf-8') as f:
